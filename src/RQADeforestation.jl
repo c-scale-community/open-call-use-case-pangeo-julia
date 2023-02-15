50a3e72734e1911444e3facfe49a261cba9e128f
Felix <felix.cremer@bgc-jena.mpg.de>
2023-02-15 10:42:45 +0100

    Files generated by PkgTemplates

---
+++ src/RQADeforestation.jl
@@ -0,0 +1,5 @@
+module RQADeforestation
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using RQADeforestation
+using Test
+
+@testset "RQADeforestation.jl" begin
+    # Write your tests here.
+end
