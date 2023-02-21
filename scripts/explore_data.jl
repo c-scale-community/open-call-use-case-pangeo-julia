c6994366fa123e8dd25b364796886ee2e7db6b76
Felix <felix.cremer@bgc-jena.mpg.de>
2023-02-21 15:29:31 +0100

    Add distributed workflow in explore_data.jl

--- scripts/explore_data.jl
+++ scripts/explore_data.jl
@@ -1,6 +1,18 @@
 using RQADeforestation
 using YAXArrays
+Threads.nthreads()
 
+using Distributed
+addprocs(7)
+@everywhere begin
+    using Pkg
+    Pkg.activate(".")
+end
+@everywhere begin
+    using ArchGDAL
+    using YAXArrays
+    using RQADeforestation
+end
 indir, pol = "/eodc/products/eodc.eu_sentinel1_backscatter/S1_CSAR_IWGRDH/SIG0/V1M1R1/EQUI7_EU020M/E048N018T3","VH"
 
 cubevh = gdalcube(indir, pol)
@@ -8,3 +20,5 @@ cubevh = gdalcube(indir, pol)
 subcube = cubevh[X=(4.8e6,4.81e6), Y=(2e6,1.99e6)]
 
 valsub = RQADeforestation.countvalid(subcube)
+
+#valcube = RQADeforestation.countvalid(cubevh)
